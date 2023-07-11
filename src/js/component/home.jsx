43380d7321a37acf4d32c8ec2bae572f58a5fcad
DanielNunez04 <130614771+DanielNunez04@users.noreply.github.com>
2023-07-11 00:03:27 +0000

    Fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,101 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { func } from "prop-types";
 
 //create your first component
 const Home = () => {
-	return (
+
+	const [tarea, setTarea] = useState("")
+	const [lista, setLista] = useState([])
+
+	const crearUsuario = async() => {
+		const response = await fetch ("https://assets.breatheco.de/apis/fake/todos/user/dnumon", {
+			method: "POST" , 
+			body: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json", 
+			} 
+		})
+		const data = await response.json ()
+		console.log(data)
+	}
+
+	useEffect(() => {
+		// crearUsuario()
+		obtenerListaTareas ()
+	},[])
+
+	useEffect(() => {
+		// crearUsuario()
+		actualizarListaTareas ()
+	},[lista])
+
+	const obtenerListaTareas = async() => {
+		try{
+			const response = await fetch ("https://assets.breatheco.de/apis/fake/todos/user/dnumon")
+			const data = await response.json ()
+			console.log(data)
+			setLista(data)
+		} catch(error){
+			console.log(error)
+		}
+	}
+
+	const actualizarListaTareas = async() => {
+		try{
+			const response = await fetch ("https://assets.breatheco.de/apis/fake/todos/user/dnumon", {
+				method: "PUT",
+				body: JSON.stringify(lista),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			const data = await response.json ()
+		}
+		catch (error){
+			console.log(error)
+		}
+	}
+
+	function envioTarea(e){
+		e.preventDefault()
+		setLista([...lista, {"label": tarea, "done": false }])
+		setTarea("")
+		// actualizarListaTareas()
+	}
+
+    function eliminar(id) {
+		let tareaFinal = [] 
+		tareaFinal = lista.filter((item, index) => {
+			if(index != id){
+				return item
+			}
+		})
+		setLista(tareaFinal)
+	}
+ 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Lista de tareas</h1>
+			<input 
+			className="form-control"
+			type="text" 
+			value={tarea}
+			onChange={(e) => setTarea(e.target.value)}/>
+			<button onClick={envioTarea}>Add task</button>
+			<br />
+			<div style={{marginTop: "10px"}}>
+			<ul className="list-group">
+				{lista.map((item,id) => (
+					<li className="list-group-item" key={id}>
+						{item.label}
+						<button className="btn btn-outline-secondary float-end"
+						onClick={() => eliminar(id)}><i className="fa fa-trash"></i></button>
+					</li>
+				))}
+			</ul>
+			</div>
 		</div>
 	);
 };
